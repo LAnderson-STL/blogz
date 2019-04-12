147399d1997a0920e7e9f8c39199061b46f85f54
LAnderson-STL <kirsch_17@hotmail.com>
2019-04-12 10:37:54 -0500

    set up templates

--- main.py
+++ main.py
@@ -1,43 +1,22 @@
+    
 from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
-#app.config['SQLALCHEMY_ECHO'] = True
-#db = SQLAlchemy(app)
 
+blogs = [] 
 
-class Blog(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(500))
-
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
-
-
-#good here
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/newpost', methods=['POST', 'GET'])
 def index():
 
-    #if request.method == 'POST':
-       # task_name = request.form['task']
-        #new_task = Task(task_name)
-        #db.session.add(new_task)
-       # db.session.commit()
+    if request.method == 'POST':
+        blog_title = request.form['blog-title']
+        blog_content = request.form['blog-content']
+        blogs.append((blog_title, blog_content))
 
-    #tasks = Task.query.filter_by(completed=False).all()
-    #completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    return render_template('newpost.html',title="Add Blog Entry", blogs=blogs)
 
-#to here
-
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['POST', 'GET'])
+def show_blog_posts():
+    return render_template('blog.html', title="Show blog Posts", blogs=blogs )
+app.run()
