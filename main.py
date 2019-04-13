9e50bbebaa95c10d8c9da1640f4451338fddb780
LAnderson-STL <kirsch_17@hotmail.com>
2019-04-13 16:51:30 -0500

    commit

--- main.py
+++ main.py
@@ -13,38 +13,44 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120)) 
     body = db.Column(db.String(500))
-    deleted = db.Column(db.Boolean, default=False)
+    
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
-        self.completed = False
 
-
-
-@app.route('/newpost', methods=['POST', 'GET'])
+def not_empty(input):
+    if input:
+        return True
+        
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    blogs = Blog.query.all()
+    return render_template('blog.html', title="Show blog Posts", blogs=blogs )
+
+
+#display form to add new posts
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_new_post():
 
     if request.method == 'POST':
         blog_title = request.form['blog-title']
         blog_body = request.form['blog-body']
         new_blog = Blog(blog_title, blog_body)
+        
+
+    
         db.session.add(new_blog)
         db.session.commit()
-
+       
+            
     blogs = Blog.query.all()
     
-
-    #this should redirect to ?!!!
-    #return redirect('/')
     return render_template('newpost.html',title="Add Blog Entry", blogs=blogs)
+
     
-
-@app.route('/', methods=['POST', 'GET'])
-def show_blog_posts():
-    blogs = Blog.query.all()
-    return render_template('blog.html', title="Show blog Posts", blogs=blogs )
-
+    
+    
 #delete blog posts
 @app.route('/delete-post', methods=['POST'])
 def delete_post():
@@ -52,6 +58,7 @@ def delete_post():
     blog = Blog.query.get(blog_id)
     db.session.delete(blog)
     db.session.commit()
+
     return redirect('/')
 
 if __name__ == '__main__':
